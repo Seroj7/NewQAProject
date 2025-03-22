2b2814ba544828f6e639c3d4596ee1f74f123f5b
Seroj Manukyan <seroj.manukyan.y@mail.ru>
2025-03-22 23:21:43 +0400

    LoginPage, navigationBar and searchresultPage done!

---
+++ tests/loginTest.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from pages.loginPage import LoginPage
+from time import sleep
+
+
+class LoginTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.delete_all_cookies()
+        self.driver.implicitly_wait(10)
+        self.driver.get("https://www.amazon.com/ap/signin?ie=UTF8&ie=UTF8&openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.com%3F&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&fromAuthPrompt=1&switch_account=signin&ignoreAuthState=1&disableLoginPrepopulate=1&ref_=ap_sw_aa")
+        self.loginPageObj = LoginPage(self.driver)
+
+
+    def test_positive_login_test(self):
+        self.loginPageObj.fill_login_field("manukyanedgars@gmail.com")
+        self.loginPageObj.click_to_continue_button()
+        self.loginPageObj.fill_password_field("AAB0663634")
+        self.loginPageObj.click_to_sign_in_button()
+        sleep(2)
+
+    def tearDown(self):
+        self.driver.close()
---
+++ tests/navigationBarTest.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+from pages.navigationBar import NavigationBar
+
+
+class NavigationBarTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.driver.delete_all_cookies()
+        self.driver.get("https://www.amazon.com/ref=nav_logo")
+        self.navigationBarObj = NavigationBar(self.driver)
+    def test_search(self):
+        self.navigationBarObj.fill_search_field("AGV Helmet")
+        self.navigationBarObj.click_to_search_button()
+        self.navigationBarObj.click_to_cart_button()
+        self.navigationBarObj.mouse_move_to_account_button()
+
+    def tearDown(self):
+        self.driver.close()
+
